7692f3f7a6c4bd3a493765156249a5a6c88a1792
Josia Almeida <josiaalmeida0001@gmail.com>
2021-09-11 11:23:39 +0100

    Exercicio 9

---
+++ Exerc9/Exe9.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+
+int main(){
+    float racaoKilo, racaoGrama;
+
+    printf("Quantidade em Kilo comprada: ");
+    scanf("%f", &racaoKilo);
+
+    printf("Consumo diario total em gramas para os dois gatos: ");
+    scanf("%f", &racaoGrama);
+
+
+    racaoKilo *=1000;
+    float Consumo5day = racaoGrama * 5;
+
+    if(Consumo5day >= racaoKilo)
+        printf("Não restou racao depois de 5 dias\n");
+    else{
+
+        float consumoFinal = racaoKilo - Consumo5day;
+
+        printf(" %.2f kilos\n", racaoKilo);
+
+        if(consumoFinal >= 1000)
+            printf("Depois de 5 dias sobrará %.2f kilos\n", consumoFinal);
+        else
+            printf("Depois de 5 dias sobrará %.2f gramas \n", consumoFinal);
+    }
+    return 0;
+}
