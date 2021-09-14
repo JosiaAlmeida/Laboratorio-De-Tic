2e9d8cf81a59b6e9a9d8a70ad47ee17c09b02b6c
JosiaAlmeida <josiaalmeida0001@gmail.com>
2021-09-14 22:29:29 +0100

    update

--- Exerc7/Exe7.c
+++ Exerc7/Exe7.c
@@ -1,7 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
+int main()
+{
+    float precoFabrica, Lucro, imposto, precoFabrica_final;
+    int percentagemLucro, percentagemImposto;
 
+    printf("Digite o lucro, imposto e o seu preço ");
+    scanf("%d%d%f", &percentagemLucro, &percentagemImposto, &precoFabrica);
+
+    Lucro = (precoFabrica * percentagemLucro / 100);
+    imposto = (precoFabrica * percentagemImposto / 100);
+
+    precoFabrica_final = (precoFabrica + Lucro - imposto);
+
+    printf("o preco final do veiculo e %.2f kz\n", precoFabrica_final);
     printf("\n");
     return 0;
 }
